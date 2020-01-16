8bc3d9e06262076ad4259bd294c059aa411ab6b3
Mitch Duncan <mitch@mitchduncan.com>
2020-01-16 12:41:50 -0700

    removed S3 bucket

--- simple/main.tf
+++ simple/main.tf
@@ -3,12 +3,7 @@ provider "aws" {
   version = "~> 2.0"
 }
 
-resource "aws_s3_bucket" "mdlths1982bucke" {
-  bucket = "mdlthsnewbucket1982"
-  acl    = "public-read"
-}
-
-resource "aws_instance" "example" {
+resource "aws_instance" "mdexample" {
   ami           = "ami-2757f631"
   instance_type = "t2.micro"
 }
