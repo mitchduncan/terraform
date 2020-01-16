c3c341406b2b83818df85f9cb4dcf674981605cd
Mitch Duncan <mitch@mitchduncan.com>
2020-01-16 12:30:25 -0700

    added a t2.micro instance

--- simple/main.tf
+++ simple/main.tf
@@ -7,3 +7,8 @@ resource "aws_s3_bucket" "mdlths1982bucke" {
   bucket = "mdlthsnewbucket1982"
   acl    = "public-read"
 }
+
+resource "aws_instance" "example" {
+  ami           = "ami-2757f631"
+  instance_type = "t2.micro"
+}
