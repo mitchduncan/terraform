b9a9fd837f110a1fbbadfb3553a45da03daff588
Mitch Duncan <mitch@mitchduncan.com>
2020-01-16 14:06:55 -0700

    deleted folder

--- main.tf
+++ main.tf
@@ -7,3 +7,8 @@ resource "aws_instance" "mdtestvm2" {
   ami           = "ami-2757f631"
   instance_type = "t2.micro"
 }
+
+resource "aws_s3_bucket" "mdlthsbucketname401" {
+  bucket = "mdlthsnewbucketprice"
+  acl    = "public-read"
+}
