34eea9cc0f1fd2aa38890ac0b992fafda62c4c18
Mitch Duncan <mitch@mitchduncan.com>
2020-01-16 12:46:07 -0700

    updated VM name

--- simple/main.tf
+++ simple/main.tf
@@ -3,7 +3,8 @@ provider "aws" {
   version = "~> 2.0"
 }
 
-resource "aws_instance" "mdexample" {
+resource "aws_instance" "mdtestvm" {
   ami           = "ami-2757f631"
   instance_type = "t2.micro"
 }
+
